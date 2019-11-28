5e49da0b2123be3a175d37fd66444a8b5f67466c
keni7385 <andrea.corsini@outlook.com>
2019-11-28 17:56:22 +0200

    Added results output function

--- app/data/task.py
+++ app/data/task.py
@@ -1,5 +1,7 @@
 import networkx as nx
 from app.evaluation.goodness import balanced_partition
+import logging
+import os
 
 
 class GraphPartitioningTask:
@@ -15,12 +17,26 @@ class GraphPartitioningTask:
         self.k = k
         self.graph = graph
 
-    def solve(self, algorithm, objective_function=balanced_partition):
-        print("[Starting] to solve {%s} for k=%d clusters\n" % (self.name, self.k))
-
+    def solve(self, algorithm, objective_function=balanced_partition, output_directory="results", save_results=True):
+        logging.info("[Starting] to solve {%s} for k=%d clusters with algorithm {%s}",
+                     self.name, self.k, algorithm.name)
         partitioned = algorithm.run(self.graph, self.k)
-        score = objective_function(partitioned, self.k)
+        logging.info("[Done] {%s} returned.", algorithm.name)
 
-        print("[Finished] Score obtained = %f \n" % score)
-        # TODO: write results to output file
-        print("[Output Done] results on file %s\n" % "fake.txt")
+        logging.info("[Starting] clustering evaluation")
+        score = objective_function(partitioned, self.k)
+        logging.info("[Finished] Score obtained = %f", score)
+
+        if save_results:
+            self._save_results(output_directory, partitioned)
+
+    def _save_results(self, output_directory, partitions):
+        if not os.path.exists(output_directory):
+            os.makedirs(output_directory)
+        filename = output_directory + os.sep + self.name + ".txt"
+        with open(filename, 'w') as f:
+            f.write("# %s %d %d %d\n" %
+                    (self.name, self.graph.number_of_nodes(), self.graph.number_of_edges(), self.k))
+            for node, data in partitions.nodes(data=True):
+                f.write("%s %d\n" % (node, data.get('partition')))
+        logging.info("[Output Done] results on file %s\n" % filename)
