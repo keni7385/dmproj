8259c0118aacd58b8b0c0a5f96ce499c8a9fe445
keni7385 <andrea.corsini@outlook.com>
2019-12-02 21:47:59 +0200

    Prepared grid search norm and not norm

--- main.py
+++ main.py
@@ -11,21 +11,22 @@ logging.getLogger().setLevel(logging.INFO)
 files = ["ca-GrQc", "Oregon-1", "soc-Epinions1", "web-NotreDame", "roadNet-CA"]
 path = "graphs_processed/%s.txt"
 output_directory = "results"
-paths = [path % file for file in files[0:2]]
+paths = [path % file for file in files]
 max_offset = 30
 negative_offset = 4
 
-for filepath in paths:
-    print("Started {}".format(filepath))
-    task_params = Reader.read(filepath)
-    embedding = Reader.load_embedding(output_directory, task_params, max_offset, negative_offset,
-                                      normalised=True)
+for normalised in [True, False]:
+    for filepath in paths:
+        print("Started {}".format(filepath))
+        task_params = Reader.read(filepath)
+        embedding = Reader.load_embedding(output_directory, task_params, max_offset, negative_offset,
+                                          normalised=normalised)
 
-    for offset in range(max_offset):
-        task_params["offset"] = offset - negative_offset
-        task = GraphPartitioningTask(**task_params)
-        upper_bound = task_params["k"] + offset - negative_offset
-        if upper_bound == 1:
-            task.solve(SpectralClustering(embedding[:, 2].reshape(-1, 1)))
-        elif upper_bound > 1:
-            task.solve(SpectralClustering(embedding[:, :upper_bound]))
+        for offset in range(max_offset):
+            task_params["offset"] = offset - negative_offset
+            task = GraphPartitioningTask(**task_params)
+            upper_bound = task_params["k"] + offset - negative_offset
+            if upper_bound == 1:
+                task.solve(SpectralClustering(embedding[:, 2].reshape(-1, 1)))
+            elif upper_bound > 1:
+                task.solve(SpectralClustering(embedding[:, :upper_bound]))
