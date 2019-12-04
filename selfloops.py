8b3bbd8ecf5f77c45a00d5b2263ef71197762b78
keni7385 <andrea.corsini@outlook.com>
2019-12-04 16:52:30 +0200

    Self edges analysis

---
+++ selfloops.py
@@ -0,0 +1,19 @@
+import networkx as nx
+from app.data.reader import Reader
+
+files = ["ca-GrQc", "Oregon-1", "soc-Epinions1", "web-NotreDame", "roadNet-CA"]
+path = "graphs_processed/%s.txt"
+output_directory = "results"
+paths = [path % file for file in files]
+
+for file in paths:
+    task = Reader.read(file)
+    print("Selfloops in %s: [%d]" % (task["name"], sum(1 for i in nx.selfloop_edges(task["graph"]))))
+exit(0)
+
+# Results:
+# Selfloops in ca-GrQc: [6]
+# Selfloops in Oregon-1: [0]
+# Selfloops in soc-Epinions1: [0]
+# Selfloops in web-NotreDame: [27455]
+# Selfloops in roadNet-CA: [0]
