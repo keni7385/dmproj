f0fb564ff7038e68d18d6d2aa289e7d62a225d3d
fabiolatorre <fabio_latorre@live.it>
2019-12-04 20:46:36 +0200

    updated README and random_state range

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 Algorithmic methods for data analysis - Graph-partitioning
 
 ## Scores
- - ca-GrQc.txt : best score with not normalized and k+2, random_state=1
+ - ca-GrQc.txt : best score with not normalized=True and k+2, random_state=1
  - Oregon-1 : best score with normalized=False, k+12, kmeans random_state=3
+ - soc-Epinions1: best score with normalized=False for, k+17, random_state=4 = 0.5503901002583569
--- main.py
+++ main.py
@@ -26,12 +26,13 @@ for filepath in paths:
     # graph_no_loops = task_params["graph"].copy()
     # graph_no_loops.remove_edges_from(nx.selfloop_edges(task_params["graph"]))
     curr_smallest_value = inf
-    for normalised in [True, False]:
+    # for normalised in [True, False]:
+    for normalised in [False]:
         embedding = Reader.load_embedding(output_directory, task_params, max_offset, negative_offset,
                                           normalised=normalised)
         # embedding = compute_eigenvectors(graph_no_loops, task_params["k"] + max_offset - negative_offset, normalised)
 
-        for random_state in range(10):
+        for random_state in range(10, 20):
             # Try different random states for the k-means algorithm
             os.environ["random_state"] = str(random_state)
 
