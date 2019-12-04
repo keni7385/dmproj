de83f4a594d6b60684cdef943a149660796e4a53
keni7385 <andrea.corsini@outlook.com>
2019-12-04 20:17:49 +0200

    Added variable number of components

--- app/algorithms/faster_spectral_custering.py
+++ app/algorithms/faster_spectral_custering.py
@@ -1,16 +1,19 @@
 import networkx as nx
-from sklearn.cluster import SpectralClustering
+from sklearn.cluster.spectral import SpectralClustering
+import os
 
 
 class FasterSpectralClustering:
 
-    def __init__(self):
+    def __init__(self, offset):
         self.name = "FasterSpectralClustering"
+        self.offset = offset
 
     def run(self, graph: nx.Graph, k: int):
         pred_k = SpectralClustering(n_clusters=k,
                                     eigen_solver="amg",
-                                    random_state=1,
+                                    random_state=int(os.environ["random_state"]),
+                                    n_components=self.offset+k,
                                     affinity="precomputed",
                                     n_jobs=-1).fit_predict(nx.adjacency_matrix(graph))
         print("Done, partitioning now...\n")
--- main_fasterspect.py
+++ main_fasterspect.py
@@ -14,20 +14,19 @@ files = ["ca-GrQc", "Oregon-1", "soc-Epinions1", "web-NotreDame", "roadNet-CA"]
 path = "graphs_processed/%s.txt"
 output_directory = "results"
 paths = [path % file for file in files]
-max_offset = 1
+max_offset = 10
 negative_offset = 0
 
-for filepath in paths[1:2]:
+for filepath in paths[3:5]:
     print("Started {}".format(filepath))
     task_params = Reader.read(filepath)
     curr_smallest_value = inf
-    for random_state in range(1):
+    for random_state in range(10):
         # Try different random states for the k-means algorithm
         os.environ["random_state"] = str(random_state)
 
         for offset in range(max_offset):
             task_params["offset"] = offset - negative_offset
             task = GraphPartitioningTask(**task_params)
-            upper_bound = task_params["k"] + offset - negative_offset
-            curr_smallest_value = task.solve(FasterSpectralClustering(),
+            curr_smallest_value = task.solve(FasterSpectralClustering(offset-negative_offset),
                                              curr_smallest_value=curr_smallest_value, normalised=True)
